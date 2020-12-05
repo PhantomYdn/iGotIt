ed8232c2b6fea4c8ceab81917a3f82c920c7c0be
Ilia Naryzhny <phantom@ydn.ru>
2020-12-05 11:58:53 -0800

    Initial commit of generated project from template for iGotIt

---
+++ src/main/java/igotit/tech/DataModel.java
@@ -0,0 +1,26 @@
+package igotit.tech;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.AbstractOrienteerModule;
+import org.orienteer.core.util.OSchemaHelper;
+
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+
+public class DataModel extends AbstractOrienteerModule{
+
+	protected DataModel() {
+		super("app", 1);
+	}
+	
+	@Override
+	public ODocument onInstall(OrienteerWebApplication app, ODatabaseSession db) {
+		super.onInstall(app, db);
+		OSchemaHelper helper = OSchemaHelper.bind(db);
+		//Install data model
+		//Return null of default OModule is enough
+		return null;
+	}
+	
+}
