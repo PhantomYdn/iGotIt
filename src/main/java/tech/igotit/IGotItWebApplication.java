e435398300c36d26d23ff850fc75ab8783046187
Ilia Naryzhny <phantom@ydn.ru>
2020-12-09 10:14:10 -0800

    Retarget JNPM to Orienteer runtime directory

--- src/main/java/tech/igotit/IGotItWebApplication.java
+++ src/main/java/tech/igotit/IGotItWebApplication.java
@@ -1,7 +1,10 @@
 package tech.igotit;
 
+import java.nio.file.Paths;
+
 import org.orienteer.core.OrienteerWebApplication;
 import org.orienteer.core.module.PerspectivesModule;
+import org.orienteer.core.util.StartupPropertiesLoader;
 import org.orienteer.jnpm.JNPMService;
 import org.orienteer.jnpm.JNPMSettings;
 import org.orienteer.jnpm.cdn.CDNWicketResource;
@@ -16,7 +19,9 @@ public class IGotItWebApplication extends OrienteerWebApplication
 		registerWidgets("tech.igotit.widget");
 		registerModule(DataModel.class);
 		if(!JNPMService.isConfigured()) 
-			   JNPMService.configure(JNPMSettings.builder().build());
+			   JNPMService.configure(JNPMSettings.builder()
+					   					.homeDirectory(Paths.get(StartupPropertiesLoader.getRuntime(), ".jnpm"))
+					   					.build());
 	    CDNWicketResource.mount(this, "/cdn");
 	}
 	
